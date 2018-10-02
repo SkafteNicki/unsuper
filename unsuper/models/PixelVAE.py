493a23ce8370d96b39dd6cfd1c2f72174b4e982e
Nicki Skafte <skaftenicki@gmail.com>
2018-10-02 08:27:57 +0200

    pixelvae

---
+++ unsuper/models/PixelVAE.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 21 08:19:43 2018
+
+@author: nsde
+"""
+
+#%%
+import torch
+
+#%%
+class PixelVAE:
+    def __init__(self, ):
+        pass
+    
+    def train(self, ):
+        pass
