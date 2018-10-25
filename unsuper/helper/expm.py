8c42371276255f24c37ef31a405bc3fc53965adf
Nicki Skafte <skaftenicki@gmail.com>
2018-10-25 11:23:07 +0200

    expm analytic is somewhat buggy

--- unsuper/helper/expm.py
+++ unsuper/helper/expm.py
@@ -90,6 +90,54 @@ def torch_expm3x3(A):
     return expmA
 
 #%%
+def torch_expm(A):
+    """ """
+    n_A = A.shape[0]
+    A_fro = torch.sqrt(A.abs().pow(2).sum(dim=(1,2), keepdim=True))
+    
+    # Scaling step
+    maxnorm = torch.Tensor([5.371920351148152]).type(A.dtype).to(A.device)
+    zero = torch.Tensor([0.0]).type(A.dtype).to(A.device)
+    n_squarings = torch.max(zero, torch.ceil(torch_log2(A_fro / maxnorm)))
+    Ascaled = A / 2.0**n_squarings    
+    n_squarings = n_squarings.flatten().type(torch.int32)
+    
+    # Pade 13 approximation
+    U, V = torch_pade13(Ascaled)
+    P = U + V
+    Q = -U + V
+    R, _ = torch.gesv(P, Q) # solve P = Q*R
+    
+    # Unsquaring step
+    expmA = [ ]
+    for i in range(n_A):
+        l = [R[i]]
+        for _ in range(n_squarings[i]):
+            l.append(l[-1].mm(l[-1]))
+        expmA.append(l[-1])
+    
+    return torch.stack(expmA)
+
+#%%
+def torch_log2(x):
+    return torch.log(x) / torch.log(torch.Tensor([2.0])).type(x.dtype).to(x.device)
+
+#%%    
+def torch_pade13(A):
+    b = torch.Tensor([64764752532480000., 32382376266240000., 7771770303897600.,
+                      1187353796428800., 129060195264000., 10559470521600.,
+                      670442572800., 33522128640., 1323241920., 40840800.,
+                      960960., 16380., 182., 1.]).type(A.dtype).to(A.device)
+        
+    ident = torch.eye(A.shape[1], dtype=A.dtype).to(A.device)
+    A2 = torch.matmul(A,A)
+    A4 = torch.matmul(A2,A2)
+    A6 = torch.matmul(A4,A2)
+    U = torch.matmul(A, torch.matmul(A6, b[13]*A6 + b[11]*A4 + b[9]*A2) + b[7]*A6 + b[5]*A4 + b[3]*A2 + b[1]*ident)
+    V = torch.matmul(A6, b[12]*A6 + b[10]*A4 + b[8]*A2) + b[6]*A6 + b[4]*A4 + b[2]*A2 + b[0]*ident
+    return U, V
+
+#%%
 if __name__ == '__main__':
     from scipy.linalg import expm
     import numpy as np
@@ -100,5 +148,5 @@ if __name__ == '__main__':
     expm_scipy = np.zeros_like(A)
     for i in range(n):
         expm_scipy[i] = expm(A[i].numpy())
-    expm_torch = torch_expm3x3(A)
-    print('Difference: ', np.linalg.norm(expm_scipy[:,:2,:] - expm_torch))
+    expm_torch = torch_expm(A)
+    print('Difference: ', np.linalg.norm(expm_scipy - expm_torch))
--- unsuper/helper/spatial_transformer.py
+++ unsuper/helper/spatial_transformer.py
@@ -9,7 +9,16 @@ Created on Fri Oct 12 12:04:43 2018
 import torch
 from torch import nn
 from torch.nn import functional as F
-from .expm import torch_expm3x3 as expm
+from .expm import torch_expm
+
+#%%
+def expm(theta): 
+    n_theta = theta.shape[0] 
+    zero_row = torch.zeros(n_theta, 1, 3, dtype=theta.dtype, device=theta.device) 
+    theta = torch.cat([theta, zero_row], dim=1) 
+    theta = torch_expm(theta) 
+    theta = theta[:,:2,:] 
+    return theta 
 
 #%%
 class STN_Affine(nn.Module):
@@ -37,7 +46,17 @@ class STN_AffineDiff(nn.Module):
         grid = F.affine_grid(theta, output_size)
         x = F.grid_sample(x, grid)
         return x
+
+#%%
+class STN_AffineSpecial(nn.Module):
+    def __init__(self, input_shape):
+        super(STN_AffineSpecial, self).__init__()
+        self.input_shape = input_shape
         
+    def forward(self, x, theta, inverse=False):
+        if inverse:
+            angle = 
+    
 #%%
 if __name__ == '__main__':
     pass
